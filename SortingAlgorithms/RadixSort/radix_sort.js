287b1cf32f555c9852f1b47a0c5a863c0aa8fc9b
Maksym Tykhonenko <m.tykhonenko@mrlab.ae>
2023-07-17 21:43:03 +0300

    Complete radix sort

--- SortingAlgorithms/RadixSort/radix_sort.js
+++ SortingAlgorithms/RadixSort/radix_sort.js
@@ -2,25 +2,25 @@ import maxDigits from "./max_digits.js";
 import getDigit from "./get_digit.js";
 
 const radixSort = (arr) => {
-  const temp = [];
-  for (let i = 0; i < maxDigits(arr); i++) {
+  const maxDigitsCount = maxDigits(arr);
+
+  for (let i = 0; i < maxDigitsCount; i++) {
+    let digitBuckets = Array.from(
+      { length: 10 },
+      () => []
+    );
+
     for (let j = 0; j < arr.length; j++) {
       let currentDigit = getDigit(arr[j], i);
-      if (!temp[currentDigit]) {
-        temp[currentDigit] = [];
-      }
-      temp[currentDigit].push(arr[j]);
+      digitBuckets[currentDigit].push(arr[j]);
     }
 
-    for (let k = 0; k < arr.length; k++) {
-
-    }
-    console.log(temp);
+    arr = [].concat(...digitBuckets);
   }
 
   return arr;
 };
 
-console.log(radixSort([400, 700, 600, 300, 500]));
+console.log(radixSort([455, 23, 15, 48, 100, 1, 11, 2]));
 
 export default radixSort;
