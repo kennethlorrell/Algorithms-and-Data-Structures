53d0fbb0d3a8f1e9fe8bb3dd5e39279d14625585
deepdark <39443507+kennethlorrell@users.noreply.github.com>
2023-07-27 19:16:26 +0300

    Add required methods for work with Binary Search Tree

--- DataStructures/BinarySearchTree/BinarySearchTree.js
+++ DataStructures/BinarySearchTree/BinarySearchTree.js
@@ -6,18 +6,46 @@ class BinarySearchTree {
   }
 
   insert(value) {
+    const newNode = new Node(value);
+
     if (!this.root) {
-      return false;
+      this.root = newNode;
+      return this;
     }
 
     let current = this.root;
+
     while (current) {
-      if (value === current) {
-        return true;
-      } else if (value < current) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        if (!current.right) {
+          current.right = newNode;
+
+          return this;
+        }
         current = current.right;
       } else {
+        return undefined;
+      }
+    }
+  }
+
+  find(value) {
+    let current = this.root;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      } else if (current.value > value) {
         current = current.left;
+      } else {
+        current = current.right;
       }
     }
 
